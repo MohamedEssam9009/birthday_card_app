70e2ae7b770beb83a38488b58dd5867300bb60e9
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-01-28 00:07:32 +0200

    Remove Checked Mode Banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class BirthdayCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Birthday Card',
       home: Scaffold(
         backgroundColor: Color(0xFFD2BCD5),
